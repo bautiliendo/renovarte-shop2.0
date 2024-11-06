28e28f8d5a255f2a8a4caaccd3d20bdee2e5e232
bautiliendo <juanbautistaliendo1@gmail.com>
2024-11-06 00:06:18 -0300

    tienda-renovarte-2.0

---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,9 @@
+// import { titleFont } from "@/config/fonts";
+
+export default function Account() {
+    return (
+        <div>
+            <h1>Admin page</h1>
+        </div>
+    );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export * from "./ui/top-menu/TopMenu";
+export * from "./ui/not-found/PageNotFound";
+export * from "./ui/title/Title";
+export * from "./products/products-grid/ProductGrid";
+export * from "./ui/sidebar/Sidebar";
